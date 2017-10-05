f506b68ba8cf55a414438305cecaab521423a0a8
Bryan <bryanporras2033@gmail.com>
2017-10-05 00:41:47 -0400

    started server and added html and css to test.html

--- server.js
+++ server.js
@@ -1,4 +1,18 @@
 var express = require("express");
-var methodOveride = require("method-override");
+var exphbs = require("express-handlebars");
 var bodyParser = require("body-parser");
 var app = express();
+
+PORT = process.env.PORT || 8080
+
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+require("./routes/htmlRoutes.js")(app);
+
+app.listen(PORT, function(){
+    console.log("Currently listening to localhost:" + PORT);
+})
