2f29004e5c17961a67b82e07550512260f9ad4a4
Bryan <bryanporras2033@gmail.com>
2017-10-06 02:16:40 -0400

    selected new sushi goes to eaten sushi still need to remove selected sushi from new sushi list

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -25,7 +25,6 @@ module.exports = function(app){
 
         eatenSushi.push(userBody);
         console.log("EATEN SUSHI: " + userBody.sushi);
-        newSushi.pop(userBody.sushi);
-        console.log(newSushi);
-    })
+        res.json(eatenSushi);
+    });
 }
