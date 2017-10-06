205217922ec9b4fdb5287e0a411315acc4c7a747
Bryan <bryanporras2033@gmail.com>
2017-10-06 02:00:44 -0400

    working on pushing to eaten Sushi, server side section works

---
+++ data/eatenSushi.js
@@ -0,0 +1,5 @@
+var eatenSushi = [
+
+];
+
+module.exports = eatenSushi;
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -6,13 +6,26 @@ module.exports = function(app){
 
 
     var newSushi = require("../data/newSushi.js")
+    var eatenSushi = require("../data/eatenSushi.js")
 
     app.post('/api/new', function(req, res){
 
         var userBody = req.body
 
         newSushi.push(userBody);
-        console.log(newSushi);
+        // console.log("NEW SUSHI: " + userBody.sushi);
         res.json(newSushi);
+        // console.log(newSushi);
     });
+
+    app.post('/api/eaten', function(req, res){
+
+        var userBody = req.body;
+        // console.log(userBody)
+
+        eatenSushi.push(userBody);
+        console.log("EATEN SUSHI: " + userBody.sushi);
+        newSushi.pop(userBody.sushi);
+        console.log(newSushi);
+    })
 }
